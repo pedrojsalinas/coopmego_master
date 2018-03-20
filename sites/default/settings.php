5737764904015f0ea9efbc4c3eeffcd38fb51f3e
Willy <willyborja95@outlook.com>
2018-03-20 09:42:40 -0500

    terminando estilos responsive frontpage

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -251,7 +251,7 @@ $databases = array (
     array (
       'database' => 'coopmego_db',
       'username' => 'root',
-      'password' => '',
+      'password' => '1234',
       'host' => '127.0.0.1',
       'port' => '3306',
       'driver' => 'mysql',
